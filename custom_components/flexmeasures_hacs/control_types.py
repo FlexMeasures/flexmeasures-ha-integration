549fd0c7392a95970db9eb8e3491050495cf8657
Victor Garcia Reolid <victor@seita.nl>
2025-01-29 16:21:51 +0100

    fix: add missing fie

---
+++ custom_components/flexmeasures_hacs/control_types.py
@@ -0,0 +1,32 @@
+"""S2 Control types for the CEM."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import timedelta
+
+
+@dataclass
+class FRBC_Config:
+    """Dataclass for FRBC configuration."""
+
+    soc_minima_sensor_id: int
+    soc_maxima_sensor_id: int
+
+    consumption_sensor_id: int
+    production_sensor_id: int
+
+    fill_level_sensor_id: int
+    fill_rate_sensor_id: int
+
+    usage_forecast_sensor_id: int
+
+    thp_fill_rate_sensor_id: int
+    thp_efficiency_sensor_id: int
+
+    nes_fill_rate_sensor_id: int
+    nes_efficiency_sensor_id: int
+
+    rm_discharge_sensor_id: int
+
+    schedule_duration: timedelta
