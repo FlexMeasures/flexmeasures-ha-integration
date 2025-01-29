bcd21c0a0eb84a7308dc51166010c48bdd61a32f
Victor Garcia Reolid <victor@seita.nl>
2025-01-29 16:22:10 +0100

    fix: add tests

---
+++ tests/test_config_flow.py
@@ -0,0 +1,59 @@
+"""Test the FlexMeasures config flow."""
+from unittest.mock import patch
+
+from homeassistant import config_entries, data_entry_flow
+from homeassistant.components.flexmeasures.config_flow import SCHEMA
+from homeassistant.components.flexmeasures.const import DOMAIN
+from homeassistant.core import HomeAssistant
+
+CONFIG = {
+    "username": "admin@admin.com",
+    "password": "admin",
+    "url": "http://localhost:5000",
+    "power_sensor": 1,
+    "soc_sensor": 3,
+    "rm_discharge_sensor_id": 4,
+    "schedule_duration": "PT24H",
+    "consumption_price_sensor": 2,
+    "production_price_sensor": 2,
+    "soc_unit": "kWh",
+    "soc_min": 0.0,
+    "soc_max": 0.001,
+}
+
+
+async def test_form(hass: HomeAssistant) -> None:
+    """Test that the form pops up on loading."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] == data_entry_flow.FlowResultType.FORM
+    assert (result["errors"] == {}) or result["errors"] is None
+
+    with patch(
+        "flexmeasures_client.FlexMeasuresClient.get_access_token",
+    ) as mock_validate_input, patch(
+        "homeassistant.components.flexmeasures.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            CONFIG,
+        )
+        await hass.async_block_till_done()
+
+        assert result2["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
+        assert result2["title"] == "FlexMeasures"
+
+        fields = {str(key): key for key in SCHEMA.schema}
+
+        for key, val in result2["options"].items():
+            if key in CONFIG:
+                assert val == CONFIG[key]
+            else:
+                assert val == fields[key].default()
+
+        mock_setup_entry.assert_called_once()
+        mock_validate_input.assert_called_once()
---
+++ tests/test_services.py
@@ -0,0 +1,100 @@
+"""Test FlexMeasures integration services."""
+
+from datetime import datetime
+from unittest.mock import patch
+
+import pytest
+from s2python.common import ControlType
+
+from homeassistant.components.flexmeasures.const import (
+    DOMAIN,
+    RESOLUTION,
+    SERVICE_CHANGE_CONTROL_TYPE,
+)
+from homeassistant.components.flexmeasures.services import time_ceil
+from homeassistant.core import HomeAssistant
+import homeassistant.util.dt as dt_util
+from homeassistant.util.dt import parse_duration
+
+
+@pytest.mark.skip(
+    reason="This test passed only when the test `test_load_unload_config_entry` does not run before."
+)
+async def test_change_control_type_service(
+    hass: HomeAssistant, fm_websocket_client
+) -> None:
+    """Test that the method activate_control_type is called when calling the service active_control_type."""
+
+    with patch(
+        "flexmeasures_client.s2.cem.CEM.activate_control_type"
+    ) as activate_control_type:
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_CHANGE_CONTROL_TYPE,
+            service_data={"control_type": "NO_SELECTION"},
+            blocking=True,
+        )
+        await hass.async_block_till_done()
+
+        activate_control_type.assert_awaited_once_with(
+            control_type=ControlType.NO_SELECTION
+        )
+
+
+async def test_trigger_and_get_schedule(
+    hass: HomeAssistant, setup_fm_integration
+) -> None:
+    """Test that the method trigger_and_get_schedule is awaited when calling the service trigger_and_get_schedule."""
+    with patch(
+        "flexmeasures_client.client.FlexMeasuresClient.trigger_and_get_schedule",
+        return_value={"values": [0.5, 0.41492, -0.0, -0.0], "unit": "MW"},
+    ) as mocked_FlexmeasuresClient:
+        await hass.services.async_call(
+            DOMAIN,
+            "trigger_and_get_schedule",
+            service_data={"soc_at_start": 10},
+            blocking=True,
+        )
+        tzinfo = dt_util.get_time_zone(hass.config.time_zone)
+        mocked_FlexmeasuresClient.assert_awaited_with(
+            sensor_id=1,
+            start=time_ceil(datetime.now(tz=tzinfo), parse_duration(RESOLUTION)),
+            duration="PT24H",
+            flex_model={
+                "soc-unit": "kWh",
+                "soc-at-start": 10,
+                "soc-max": 0.001,
+                "soc-min": 0.0,
+            },
+            flex_context={"consumption-price-sensor": 2, "production-price-sensor": 2},
+        )
+
+
+async def test_post_measurements(hass: HomeAssistant, setup_fm_integration) -> None:
+    """Test that the method post measurements is called when calling the service post_measurements."""
+
+    with patch(
+        "flexmeasures_client.client.FlexMeasuresClient.post_measurements",
+        return_value=None,
+    ) as mocked_FlexmeasuresClient:
+        await hass.services.async_call(
+            DOMAIN,
+            "post_measurements",
+            service_data={
+                "sensor_id": 1,
+                "start": None,
+                "duration": "PT24H",
+                "values": [1, 1, 1, 3],
+                "unit": "kWh",
+                "prior": None,
+            },
+            blocking=True,
+        )
+        mocked_FlexmeasuresClient.assert_called_with(
+            sensor_id=1,
+            start=None,
+            duration="PT24H",
+            values=[1, 1, 1, 3],
+            unit="kWh",
+            prior=None,
+        )
