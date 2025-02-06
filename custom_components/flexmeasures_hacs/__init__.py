5a1b9ac12bb53d8d8679023c3d0b56736ce96e46
Victor Garcia Reolid <victor@seita.nl>
2025-02-06 09:37:44 +0100

    fix: get FRBC data from section
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- custom_components/flexmeasures_hacs/__init__.py
+++ custom_components/flexmeasures_hacs/__init__.py
@@ -58,8 +58,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     frbc_data_dict = {}
 
+    non_s2_fields = [
+        "consumption_sensor_id",
+        "production_sensor_id",
+        "schedule_duration",
+    ]
+
     for field in fields(FRBC_Config):
-        frbc_data_dict[field.name] = get_from_option_or_config(field.name, entry)
+        if field.name in non_s2_fields:
+            frbc_data_dict[field.name] = get_from_option_or_config(field.name, entry)
+        else:
+            frbc_data_dict[field.name] = get_from_option_or_config(
+                field.name, entry, section="s2"
+            )
 
     FRBC_data = FRBC_Config(**frbc_data_dict)
     hass.data[DOMAIN][FRBC_CONFIG] = FRBC_data
@@ -94,3 +105,39 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id, None)
 
     return unload_ok
+
+
+async def async_migrate_entry(hass, config_entry: ConfigEntry):
+    """Migrate old entry."""
+    _LOGGER.debug(
+        "Migrating configuration from version %s.%s",
+        config_entry.version,
+        config_entry.minor_version,
+    )
+
+    if config_entry.version > 2:
+        # This means the user has downgraded from a future version
+        return False
+
+    if config_entry.version == 1:
+        from .config_flow import S2_SCHEMA
+
+        new_data = {**config_entry.data} | {**config_entry.options}
+        new_data["s2"] = {}
+
+        for field in S2_SCHEMA.schema.keys():
+            field_name = str(field)
+            if field_name in new_data:
+                new_data["s2"][field_name] = new_data[field_name]
+            elif hasattr(field, "default"):
+                new_data["s2"][field_name] = field.default()
+
+        hass.config_entries.async_update_entry(config_entry, data=new_data, version=2)
+
+    _LOGGER.debug(
+        "Migration to configuration version %s.%s successful",
+        config_entry.version,
+        config_entry.minor_version,
+    )
+
+    return True
