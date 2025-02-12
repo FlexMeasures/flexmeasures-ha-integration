625da075fcca8e8f0efa37d3c3c9f7c234dfaac5
Victor <victor@seita.nl>
2025-02-12 12:01:46 +0100

    Chore/move integration from core to hacs (#10)
    * feat: add new labels
    
    * fix: update setup entry
    
    * fix: update config flow and websockets
    
    * fix: add missing fie
    
    * fix: add tests
    
    * chore: update .gitignore with IDE files
    
    * ci: add test deps
    
    * fix: make tests to work with the custom integration
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: add __init__ to folder custom_components
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * ci: test ci
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * style: apply pre-commit
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * ci: fix install test requirements
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * ci: fix use the right version for fm-client
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: make most of the config attributes optional
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * chore: upgrade version
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * ci: use latest version of fm-client in test step
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * test: improve test name and docstring
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * ci: use add fm[s2]
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * ci: use add fm[s2]
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * deps: use 0.3.3
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * deps: use 0.3.3
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: typo
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix comment
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * update en.json with the strings corresponding to the section s2
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: add ID for testing
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * chore: remove pin of fm-client and move to homeassitant 2025
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * feat: add section for the S2 settings
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: look up section if provided
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: get FRBC data from section
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * test: add test for the migration step
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * chore: change HA version
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * chore: add beta tag
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: fix beta tag
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: upgrade fm-client
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * chore: upgrade fm-client
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: use CEM version as the only supported version
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * chore: change version
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * typo
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: temporary fix to avoid the integration to reload indefinitely
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: add S2 section english strings
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    * fix: remove s2 field from reauth_confirm data description
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    
    ---------
    
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

---
+++ tests/test_init.py
@@ -0,0 +1,29 @@
+"""Test initialization of FlexMeasures integration."""
+
+from custom_components.flexmeasures_hacs.const import DOMAIN
+from custom_components.flexmeasures_hacs.services import SERVICES
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.core import HomeAssistant
+
+
+async def test_load_unload_config_entry(
+    hass: HomeAssistant, setup_fm_integration
+) -> None:
+    """Test setup of integration."""
+
+    entry = setup_fm_integration
+
+    assert len(hass.config_entries.async_entries(DOMAIN)) == 1
+
+    for service in SERVICES:
+        assert hass.services.has_service(DOMAIN, service["service"])
+
+    assert entry.state == ConfigEntryState.LOADED
+
+    await hass.config_entries.async_unload(entry.entry_id)
+    await hass.async_block_till_done()
+
+    for service in SERVICES:
+        assert not hass.services.has_service(DOMAIN, service["service"])
+
+    assert entry.state == ConfigEntryState.NOT_LOADED
---
+++ tests/test_websockets.py
@@ -0,0 +1,24 @@
+"""Test websockets module of FlexMeasures integration."""
+
+# pytest ./tests/components/flexmeasures/ --cov=homeassistant.components.flexmeasures --cov-report term-missing -vv
+
+from homeassistant.core import HomeAssistant
+
+
+async def test_cem_processes_websocket_msg(
+    hass: HomeAssistant, setup_fm_integration, fm_websocket_client
+):
+    """
+    Test that the CEM gets a S2 message sent via WebSockets through HomeAssistant
+    """
+    message = {
+        "message_id": "2bdec96b-be3b-4ba9-afa0-c4a0632cced3",
+        "role": "RM",
+        "supported_protocol_versions": ["0.1.0"],
+        "message_type": "Handshake",
+    }
+    await fm_websocket_client.send_json(message)
+    msg = await fm_websocket_client.receive_json()
+
+    assert msg["message_type"] == "Handshake"
+    assert msg["role"] == "CEM"
