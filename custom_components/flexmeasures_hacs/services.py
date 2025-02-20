f61fe5d9f16c41a4cfabbb14256d0f07eb880f82
Victor Garcia Reolid <victor@seita.nl>
2025-02-20 18:32:09 +0100

    fix: remove breakpoint
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- custom_components/flexmeasures_hacs/services.py
+++ custom_components/flexmeasures_hacs/services.py
@@ -158,7 +158,6 @@ async def async_setup_services(hass: HomeAssistant, entry: ConfigEntry) -> None:
     ):  # pylint: disable=possibly-unused-variable
         """Send S2 Fill Rate Based Control message to the ResourceManager"""
 
-        breakpoint()
         if "cem" not in hass.data[DOMAIN]:
             raise UndefinedCEMError()
 
